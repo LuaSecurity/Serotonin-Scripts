b88dc7ebc6f3742f74e639c3b7a03bcb66f1f92f
LuaSecurity <211252621+LuaSecurity@users.noreply.github.com>
2025-12-11 01:48:52 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -30,7 +30,7 @@ local Username = LocalPlayer.Name
 local games = {
     [537413528] = {"Build A Boat For Treasure", "BABFT.lua"},
     [286090429] = {"Arsenal", "Arsenal.lua"},
-    [112757576021097] = {"Defuse Division", "DD.lua"},
+    [155615604] = {"Prison Life", "PrisonLife.lua"},
 }
 
 local function GetGame(Id)
